55ec82a2ca3762e777a72b35ebd50603848fe639
Franz Koch <franz.koch@elements-net.de>
2014-08-14 22:06:54 +0200

    [Task] remove unused function

--- includes/functions.php
+++ includes/functions.php
@@ -3,18 +3,6 @@
 require_once('SimpleImage.php');
 
 
-//php function to check whether the url exists or not and validate it  
-function check_url($url)  
-{  
-$check = @fopen($url,"r"); // we are opening url with fopen  
-if($check)  
- $status = true;  
-else  
- $status = false;  
-   
-return $status;  
-}  
- 
 
 function getDisclaimer() {
 	$view = new TemplateView();
